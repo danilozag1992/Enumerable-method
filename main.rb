1f060436344f3109aa0ba9449aeba7de32833e9e
danilozag1992 <danilozagarcanin@gmail.com>
2019-08-10 17:17:36 +0200

    formatted code

--- main.rb
+++ main.rb
@@ -1,81 +1,74 @@
 
 module Enumerable
-    def my_each
-        for i in 0...self.length
-        yield(self[i])
-        end
+  def my_each
+    for i in 0...self.length
+      yield(self[i])
     end
-         
-    def my_each_with_index
-        k = 0;
-        for i in 0...self.length
-        yield(self[i], k)
-        k += 1
-    end
-    end
-         
-    def my_select(&block)
-        result = []
-        self.each do |element|
-        result << element if block.call(element) == true
-    end
-        result
-    end
+  end
 
-    def my_all?
-        self.my_each { |element| return false if yield(element) == false}
-        true
+  def my_each_with_index
+    k = 0
+    for i in 0...self.length
+      yield(self[i], k)
+      k += 1
     end
+  end
 
-    def my_any?
-        self.my_each { |element| return true if yield(element)}
-        false
+  def my_select(&block)
+    result = []
+    self.each do |element|
+      result << element if block.call(element) == true
     end
-        
-    def my_none?
-        result = true
-        self.each{|element| result = false if yield(element)}
-        result
-    end
+    result
+  end
 
-    def my_count(x=nil)
-        count = 0
-        if block_given?
-            self.my_each { |el| count += 1 if yield el }
-        elsif x
-            self.my_each { |el| count += 1 if el == x }
-        else
-            count = self.length
-        end
-        count
-    end
+  def my_all?
+    self.my_each { |element| return false if yield(element) == false }
+    true
+  end
 
-    def my_map(&block)
-      result = []
-      self.my_each do |i|
+  def my_any?
+    self.my_each { |element| return true if yield(element) }
+    false
+  end
+
+  def my_none?
+    result = true
+    self.each { |element| result = false if yield(element) }
+    result
+  end
+
+  def my_count(x = nil)
+    count = 0
+    if block_given?
+      self.my_each { |el| count += 1 if yield el }
+    elsif x
+      self.my_each { |el| count += 1 if el == x }
+    else
+      count = self.length
+    end
+    count
+  end
+
+  def my_map(&block)
+    result = []
+    self.my_each do |i|
       result << block.call(i)
-      end
-      result
     end
+    result
+  end
 
-    def my_inject element=0
-      result = element
-      self.my_each do |i|
+  def my_inject(element = 0)
+    result = element
+    self.my_each do |i|
       result = yield(result, i)
     end
-      result
-    end
+    result
+  end
 
-def multiply_els
-      self.my_inject(1){|x, y| x * y}
-    end
-
+  def multiply_els
+    self.my_inject(1) { |x, y| x * y }
+  end
 end
 
-   puts [2,3,1,4].my_count{|x| x%2 == 0}
-    
-
-# ary = [1, 2, 4, 2]
-# puts ary.count                  #=> 4
-# puts ary.count(2)               #=> 2
-# puts ary.count { |x| x%2 == 0 } #=> 3
+puts [2, 3, 1, 4].my_count { |x| x % 2 == 0 }
